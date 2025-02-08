b8e3c081b4955205fa2d4eb855a017fefad2b967
MewUbuntu <mewmewdevart@gmail.com>
2025-02-08 05:56:33 -0300

    feat: add new header component

---
+++ src/components/header/Header.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import './header.css';
+    export let showModal: boolean = false;
+</script>
+
+<header class="header">
+    <div class="header__container">
+        {#if showModal}
+            <img class="header__spaceship" src="./spaceship.png" alt="Rocket flying animation" />
+        {/if}
+    </div>
+</header>
