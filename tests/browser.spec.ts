d39eefb907e37bb675cff3271e937a8812c225e4
MewUbuntu <mewmewdevart@gmail.com>
2025-02-08 05:56:33 -0300

    chore: update Playwright configuration

--- tests/browser.spec.ts
+++ tests/browser.spec.ts
@@ -1,75 +1,79 @@
 import { test, expect } from '@playwright/test';
 
-test('Test all fields and functionalities of the challenge', async ({ page }) => {
-    // Navegar até a página
-    await page.goto('http://localhost:5173');
+test.describe('Challenge Page', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+    });
 
-    const name = 'Larissa Cristina Benedito';
-    const phone = '5511980491579';
-    const email = 'mewmewdevart@gmail.com';
+    test('should start the challenge and display the countdown timer when the start button is clickedd', async ({ page }) => {
+        await page.click('button:has-text("Iniciar Desafio")');
+        await expect(page.locator('.challenge__timer-label')).toBeVisible();
+    });
 
-    // INICIO ======================================= 
-    // Inicia do desafio, verifica se os campos estão vazios e preenche os campos, clica em "Enviar" e verifica se a mensagem de sucesso foi exibida no Modal
-    await page.waitForSelector('button:text("Iniciar Desafio")');
-    await page.click('button:text("Iniciar Desafio")');
-    await expect(page.locator('input[id="name-address-icon"]')).toHaveValue('');
-    await expect(page.locator('input[id="phone-address-icon"]')).toHaveValue('');
-    await expect(page.locator('input[id="email-address-icon"]')).toHaveValue('');
-    await page.waitForSelector('button:text("Enviar")');
-    await page.fill('input[id="name-address-icon"]', name);
-    await page.fill('input[id="phone-address-icon"]', phone);
-    await page.fill('input[id="email-address-icon"]', email);
-    const countdownText = await page.locator('text=Tempo restante: 00:15');
-    await expect(countdownText).toBeVisible();
-    await expect(countdownText).toContainText('00:15');
-    await page.click('button:text("Enviar")');
-    const successMessage = page.locator('text=Desafio finalizado com sucesso!');
-    await expect(successMessage).toBeVisible({ timeout: 10000 });
-    await page.click('button:text("✕")');
-    await expect(successMessage).not.toBeVisible();
+    test('Should validate form fields and show success modal when form is submitted within the time limit', async ({ page }) => {
+        await page.click('button:has-text("Iniciar Desafio")');
+        await page.fill('input[placeholder="Digite seu nome"]', 'Larissa Cristina');
+        await page.fill('input[placeholder="Digite o seu telefone"]', '(11) 94002-8922');
+        await page.fill('input[placeholder="Digite o seu email"]', 'larissa@example.com');
+        await page.click('button:has-text("Enviar")');
+        await expect(page.locator('.modal-box')).toBeVisible();
+        await expect(page.locator('.modal-box')).toContainText('Desafio finalizado com sucesso!');
+    });
 
+    test('Should show error messages and disable submit button for invalid form field', async ({ page }) => {
+        await page.goto('/');
+        await page.click('button:has-text("Iniciar Desafio")');
+        await page.fill('input[placeholder="Digite seu nome"]', 'Test User');
+        await page.fill('input[placeholder="Digite o seu telefone"]', '1234567890');
+        await page.fill('input[placeholder="Digite o seu email"]', 'invalid-email');
+        await page.click('button:has-text("Enviar")');
+        const emailInput = await page.locator('input[placeholder="Digite o seu email"]');
+        await expect(emailInput).toHaveAttribute('required', '');
+    });
 
-    // REINICIA ======================================= 
-    // Reinicia o desafio, verifica se o contador foi resetado para "00:15", verifica se os campos foram limpos, preenche os campos, verifica se o contador foi iniciado e exibe "00:15", clica em "Enviar" e verifica se a mensagem de sucesso foi exibida no Modal e clica em "Ver Candidato"
-    await page.click('button:text("Reiniciar Desafio")');
-    const resetCountdownText = page.locator('text=Tempo restante: 00:15');
-    await expect(resetCountdownText).toBeVisible();
-    await expect(resetCountdownText).toContainText('00:15');
-    await expect(page.locator('input[id="name-address-icon"]')).toHaveValue('');
-    await expect(page.locator('input[id="phone-address-icon"]')).toHaveValue('');
-    await expect(page.locator('input[id="email-address-icon"]')).toHaveValue('');
-    await page.fill('input[id="name-address-icon"]', name);
-    await page.fill('input[id="phone-address-icon"]', phone);
-    await page.fill('input[id="email-address-icon"]', email);
-    const countdownTextAfterReset = await page.locator('text=Tempo restante: 00:15');
-    await expect(countdownTextAfterReset).toBeVisible();
-    await expect(countdownTextAfterReset).toContainText('00:15');
-    await page.click('button:text("Enviar")');
-    const successMessageAfterReset = page.locator('text=Desafio finalizado com sucesso!');
-    await expect(successMessageAfterReset).toBeVisible({ timeout: 10000 });
-    await page.click('button:text("✕")');
-    await expect(successMessageAfterReset).not.toBeVisible();
-    await page.click('button:text("Ver Candidato")');
+    test('Should correctly decrement the countdown timer every second', async ({ page }) => {
+        await page.click('button:has-text("Iniciar Desafio")');
+        await expect(page.locator('.timer-minutes')).toHaveText('00');
+        await expect(page.locator('.timer-seconds')).toHaveText('15'); // Timer starts at 15 seconds
+        await page.waitForTimeout(5000); // Wait for 5 seconds
+        await expect(page.locator('.timer-seconds')).toHaveText('10'); // Timer should be at 10 seconds
+    });
 
+    test('Should show failure modal when the countdown reaches zero before form submission', async ({ page }) => {
+        await page.click('button:has-text("Iniciar Desafio")');
+        await page.fill('input[placeholder="Digite seu nome"]', 'Larissa Cristina');
+        await page.fill('input[placeholder="Digite o seu telefone"]', '(11) 94002-8922');
+        await page.fill('input[placeholder="Digite o seu email"]', 'larissa@example.com');
+        await page.waitForTimeout(16000); // wait for 16 seconds to let the countdown end
+        await expect(page.locator('.modal-box')).toBeVisible();
+        await expect(page.locator('.modal-box')).toContainText('Desafio finalizado com falha!');
+    });
 
-    await page.waitForSelector('button:text("Voltar ao Desafio")');
-    const displayedName = await page.locator('text=' + name);
-    const displayedPhone = await page.locator('text=' + phone);
-    const displayedEmail = await page.locator('text=' + email);
-    await expect(displayedName).toBeVisible();
-    await expect(displayedPhone).toBeVisible();
-    await expect(displayedEmail).toBeVisible();
-    await page.click('button:text("Voltar ao Desafio")');
+    test('Should redirect to candidate page and show a warning message if challenge is not completed', async ({ page }) => {
+        await page.click('button:has-text("Iniciar Desafio")');
+        await page.click('button:has-text("Ver Candidato")');
+        await expect(page).toHaveURL('/candidate');
+        await expect(page.locator('text=Por favor, finalize o desafio com sucesso para visualizar esta página.')).toBeVisible();
+        await page.click('button:has-text("Voltar para o Desafio")');
+        await expect(page).toHaveURL('/');
+    });
 
+    test('Should allow access to candidate page and display candidate details after successful challenge completion', async ({ page }) => {
+        await page.click('button:has-text("Iniciar Desafio")');
+        await page.fill('input[placeholder="Digite seu nome"]', 'Larissa Cristina');
+        await page.fill('input[placeholder="Digite o seu telefone"]', '(11) 94002-8922');
+        await page.fill('input[placeholder="Digite o seu email"]', 'larissa@example.com');
+        await page.click('button:has-text("Enviar")');
+        await expect(page.locator('.modal-box')).toBeVisible();
+        await expect(page.locator('.modal-box')).toContainText('Desafio finalizado com sucesso!');
+        await page.click('button[aria-label="Fechar modal"]');
+        await page.click('button:has-text("Ver Candidato")');
+        await expect(page).toHaveURL('/candidate');
+        await expect(page.locator('text=Larissa Cristina')).toBeVisible();
+        await expect(page.locator('text=(11) 94002-8922')).toBeVisible();
+        await expect(page.locator('text=larissa@example.com')).toBeVisible();
+        await page.click('button:has-text("Voltar para o Desafio")');
+        await expect(page).toHaveURL('/');
+    });
 
-    // // Aguarda o tempo passar (15 segundos)
-    // await page.waitForTimeout(16000); // Espera 16 segundos para garantir que o tempo tenha acabado
-
-    // // Verifica se a mensagem de falha foi exibida
-    // const failureMessage = page.locator('text=Desafio finalizado com falha!');
-    // await expect(failureMessage).toBeVisible();
-
-    // // Fecha o modal novamente
-    // await page.click('button:text("✕")');
-    // await expect(failureMessage).not.toBeVisible();
 });
