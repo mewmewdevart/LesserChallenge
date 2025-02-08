1f86ac94b969bef6d3dfc413e6e16b59fe479492
MewUbuntu <mewmewdevart@gmail.com>
2025-02-08 05:56:33 -0300

    chore: update Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,14 +5,21 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
 		extend: {
 			colors: {
-				'custom-purple': '#A073B0', // Add your custom color here
+				'custom-purple': '#A073B0',
+			},
+			keyframes: {
+				fly: {
+					'0%': { bottom: '-250px' },
+					'100%': { bottom: '100%' },
+				},
+			},
+			animation: {
+				fly: 'fly 2s 0.5s ease-in',
 			},
 		},
 	},
-
 	plugins: [typography, forms, daisyui],
 } satisfies Config;
