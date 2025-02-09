3d873ef148cc49967646d753149acab191a5e33b
MewUbuntu <mewmewdevart@gmail.com>
2025-02-09 20:12:04 -0300

    Add new Header component

---
+++ src/components/Header/Header.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import CloudBackground from '$components/CloudBackground/CloudBackground.svelte';
+    import './Header.css';
+    export let showModal: boolean = false;
+</script>
+
+<header class="header">
+    <div class="header__container">
+        <CloudBackground />
+        {#if showModal}
+            <img class="header__spaceship" src="./spaceship.png" alt="Rocket flying animation" />
+        {/if}
+    </div>
+</header>
