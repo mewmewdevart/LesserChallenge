20041b95d6b81a8fc088543136f196362864257c
MewUbuntu <mewmewdevart@gmail.com>
2025-02-09 20:11:49 -0300

    Update svelte.config.js with new configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
+import { sveltePreprocess } from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		}),
-		alias: {
-			$lib: './src/lib', 
-			$routes: './src/routes',
-			$stores: './src/stores',
-			$components: './src/components',
-		}
-	}
+    preprocess: [
+        sveltePreprocess(),
+        vitePreprocess()
+    ],
+    kit: {
+        adapter: adapter({
+            fallback: 'index.html'
+        }),
+        alias: {
+            $lib: './src/lib', 
+            $routes: './src/routes',
+            $stores: './src/stores',
+            $components: './src/components',
+        }
+    }
 };
 
 export default config;
